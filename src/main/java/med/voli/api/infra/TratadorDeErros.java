001aacc680da50c6c18274986f7c864834a9db52
Tiago Silva <tiagosilva0512@gmail.com>
2025-03-31 12:06:33 -0300

    Tratar erro de violação de integridade de dados

--- src/main/java/med/voli/api/infra/TratadorDeErros.java
+++ src/main/java/med/voli/api/infra/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package med.voli.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,6 +17,11 @@ public class TratadorDeErros {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity tratarErroViolacaoIntegridadeDados(DataIntegrityViolationException ex) {
+        return ResponseEntity.badRequest().body(new DadosErroViolacaoIntegridadeDados("400", ex.getLocalizedMessage()));
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
         var erros = ex.getFieldErrors();
@@ -25,6 +32,13 @@ public class TratadorDeErros {
     private record DadosErroValidacao(String campo, String mensagem) {
         public DadosErroValidacao(FieldError erro) {
             this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
+    private record DadosErroViolacaoIntegridadeDados(String erro, String mensagem) {
+        public DadosErroViolacaoIntegridadeDados(String erro, String mensagem) {
+            this.erro = erro;
+            this.mensagem = mensagem;
         }
     }
 }
